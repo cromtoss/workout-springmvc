4f66453f61d37a31d70cf456fe2c386e33d1c24b
Thomas A. Cross <tom@cromtoss.us>
2013-07-14 17:46:28 -0500

    initial commit

---
+++ src/main/java/com/devket/wotd/Test.java
@@ -0,0 +1,11 @@
+package com.devket.wotd;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: crosta
+ * Date: 7/14/13
+ * Time: 5:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
