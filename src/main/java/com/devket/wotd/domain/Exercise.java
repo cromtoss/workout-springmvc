275d43afab3453b8f37ff4e00f42c81b9d3ca3e7
Thomas A. Cross <tom@cromtoss.us>
2013-07-22 06:55:27 -0500

    started domain work

---
+++ src/main/java/com/devket/wotd/domain/Exercise.java
@@ -0,0 +1,18 @@
+package com.devket.wotd.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: crosta
+ * Date: 7/21/13
+ * Time: 5:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Exercise {
+
+    private String id;
+    private String name;
+    private ExerciseTarget exerciseTarget;
+    private String description;
+
+
+}
---
+++ src/main/java/com/devket/wotd/domain/ExerciseTarget.java
@@ -0,0 +1,15 @@
+package com.devket.wotd.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: crosta
+ * Date: 7/21/13
+ * Time: 5:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExerciseTarget {
+
+    public int id;
+    public String targetName;
+
+}
